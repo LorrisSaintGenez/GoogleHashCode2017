d3e0d81dafa541d66e392364e02477d156681719
Hadrien lamotte <hadrien.lamotte@gmail.com>
2017-02-23 21:30:10 +0100

    dcsV

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import sys
 import random
@@ -9,104 +10,133 @@ nbRequests = 0
 nbCaches = 0
 sizeEachCache = 0
 
+bigMatrix = [] #requete video, endpoint, nbrequete
+matrix2 = [] #latence / nbcache
+video_list_sizes = [] #taille des video
+master = [] #cache / latence
 
 def parseFile(file):
     global nbCaches
-    matrix = []
+    global matrix2
+    global nbVideos
+    global sizeEachCache
     process = 0
     val_parse = 0
     detail = []
-    master = []
-    video_list_sizes = []
+    global master
+    global video_list_sizes
     endpos = 0
     flag = 0
     requestMatrix = []
-    bigMatrix = []
+    global bigMatrix
     with open(file, 'r') as f:
-        for row, line in enumerate(f):
+        for (row, line) in enumerate(f):
             if row < 1:
-		nbVideos = line.split()[0]
-		nbEndPoints = line.split()[1]
-		nbRequests = line.split()[2]
-		nbCaches = line.split()[3]
-		sizeEachCache = line.split()[4]
-	    if row == 1:
-		for video in line.split(' '):
-		  video_list_sizes.append(video)
-	    if row > 1:
-		try:
-			line.split(' ')[2]
-			requestMatrix.append(line.split()[0])
-			requestMatrix.append(line.split()[1])
-			requestMatrix.append(line.split()[2])
-			bigMatrix.append(requestMatrix)
-			requestMatrix = []
+                nbVideos = line.split()[0]
+                nbEndPoints = line.split()[1]
+                nbRequests = line.split()[2]
+                nbCaches = line.split()[3]
+                sizeEachCache = line.split()[4]
+            if row == 1:
+                for video in line.split(' '):
+                    video_list_sizes.append(video)
+            if row > 1:
+                try:
+                    line.split(' ')[2]
+                    requestMatrix.append(line.split()[0])
+                    requestMatrix.append(line.split()[1])
+                    requestMatrix.append(line.split()[2])
+                    bigMatrix.append(requestMatrix)
+                    requestMatrix = []
+                except:
 
-		except:
-			if process == 0:
-				matrix.append(line.split(' ')[0] + ' ' + line.split(' ')[1])
-				val_parse = int(line.split(' ')[1])
-				process = 1
-				endpos += 1
-				if val_parse == 0:
-					flag = 1
-			else:	
-				detail.append(line.split(' ')[0] + ' ' + line.split(' ')[1])
-				val_parse -= 1
-				if val_parse == 0:
-					process = 0
-					master.append(detail)
-					detail = []
-	return video_list_sizes
+                    if process == 0:
+                        matrix2.append(line.split(' ')[0] + ' '
+                                + line.split(' ')[1][:-1])
+                        val_parse = int(line.split(' ')[1])
+                        process = 1
+                        endpos += 1
+                        if val_parse == 0:
+                            flag = 1
+                    else:
+                        detail.append(line.split(' ')[0] + ' '
+                                + line.split(' ')[1])
+                        val_parse -= 1
+                        if val_parse == 0:
+                            process = 0
+                            master.append(detail)
+                            detail = []
+        return video_list_sizes, matrix2
 
-#print(parseFile('trending_today.in'))
 
-
+# print(parseFile('trending_today.in'))
 
 def Desepoire():
-    print('trop vieux pour cette merde !')
+    print ('trop vieux pour cette merde !')
+
 
 def FromContainerToString(val):
-    tmp = ""
+    tmp = ''
     for x in val:
-        val += x + ""
+        val += x + ''
     return tmp
+
 
 def WriteToFile(header, val):
     f = open('test.txt', 'w+')
     i = 0
     f.write(str(header) + '\n')
     for x in val:
-        f.write(str(i) + " ")
+        f.write(str(i) + ' ')
         i += 1
         for y in x:
             f.write(str(y) + ' ')
         f.write('\n')
     f.close()
 
-'''a = "500" + '\n'
-for i in range(0, 500):
-    a += str(i) +' '+ str(random.randint(0,500)) + '\n'
-f = open('test.txt', 'w')
-f.write(a)
-f.close()'''
 
-#==============================================================================#
+# ==============================================================================#
 
-videos = parseFile('me_at_the_zoo.in')
-videos[len(videos) - 1] = videos[len(videos) - 1][:-1]
-#print("====" + videos[len(videos) - 1] + "====")
+videos, plop = parseFile('me_at_the_zoo.in')
+videos[len(videos) - 1] = (videos[len(videos) - 1])[:-1]
+'''
+bigMatrix = [] #requete video, endpoint, nbrequete
+matrix = [] #latence / nbcache
+video_list_sizes = [] #taille des video
+master = [] #cache / latence
+'''
+
 
 def algo():
+    currentCacheSize = 0
     tabLength = len(videos)
     matrix = []
-    if (nbCaches != 0):
-        split = tabLength // int(nbCaches)
-        matrix = [[0 for x in range(split)] for y in range(int(nbCaches))]
-        for i in range(int(nbCaches)):
-            for j in range(split):
-                matrix[i][j] = videos[i * split + j]
-    return matrix
+    cachenb = 0
+    print(nbCaches)
+    if nbCaches != 0:
+        print(int(nbVideos))
+        tmp = []
+        for nbVideo in range(int(nbVideos)):
+            #print(nbVideo)
+            if (currentCacheSize + int(video_list_sizes[nbVideo]) < int(sizeEachCache)):
+                print(str(currentCacheSize + int(video_list_sizes[nbVideo])) + "!////!" + str(int(sizeEachCache)))
+                currentCacheSize += int(video_list_sizes[nbVideo])
+                tmp.append(nbVideo)
+                #print("add video")
+            else:
+                print(tmp)
+                tmp = []
+                currentCacheSize = 0
+                matrix.append(tmp)
+                #print("add")
+                cachenb += 1
+                if (cachenb >= int(nbCaches)):
+                    return matrix
+        print(matrix)
+        return matrix
 
-#print(algo())
+
+
+var = algo()
+print(var)
 WriteToFile(nbCaches, algo())
