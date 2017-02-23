c1bd2d2d811ea4a01f556092331b25ae42d09c65
Lorris <lorris.saint-genez@epita.fr>
2017-02-23 13:47:39 +0100

    bebeb

--- parser.py
+++ parser.py
@@ -1,19 +1,69 @@
 import sys
 
+nbVideos = 0
+nbEndPoints = 0
+nbRequests = 0
+nbCaches = 0
+sizeEachCache = 0
+
+
 def parseFile(file):
     matrix = []
+    process = 0
+    val_parse = 0
+    detail = []
+    master = []
+    video_list_sizes = []
+    endpos = 0
+    flag = 0
+    requestMatrix = []
+    bigMatrix = []
     with open(file, 'r') as f:
-        intel_list = []
         for row, line in enumerate(f):
             if row < 1:
-                for column, word in enumerate(line.split()):
-                    intel_list.insert(column, word)
-                matrix = [[0 for x in range(int(intel_list[1]))] for y in range(int(intel_list[0]))]
-            else :
+		nbVideos = line.split()[0]
+		nbEndPoints = line.split()[1]
+		nbRequests = line.split()[2]
+		nbCaches = line.split()[3]
+		sizeEachCache = line.split()[4]
+	    if row == 1:
+		for video in line.split(' '):
+		  video_list_sizes.append(video)
+	    if row > 1:
+		if endpos < nbEndPoints and flag == 0:	
+			if process == 0:
+				matrix.append(line.split(' ')[0] + ' ' + line.split(' ')[1])
+				val_parse = int(line.split(' ')[1])
+				process = 1
+				endpos += 1
+				if val_parse == 0:
+					flag = 1
+			else:	
+				detail.append(line.split(' ')[0] + ' ' + line.split(' ')[1])
+				val_parse -= 1
+				if val_parse == 0:
+					process = 0
+					master.append(detail)
+					detail = []
+		else:
+			requestMatrix.append(line.split()[0])
+			requestMatrix.append(line.split()[1])
+			requestMatrix.append(line.split()[2])
+			bigMatrix.append(requestMatrix)
+			requestMatrix = []
+
+			
+            '''else :
                 for column, char in enumerate(line):
                     if column < int(intel_list[1]) :
                         matrix[row - 1][column] = char
+
     return matrix, intel_list[2], intel_list[3]
+'''	
+    return master
+
+print(parseFile('trending_today.in'))
+
 
 
 def Desepoire():
